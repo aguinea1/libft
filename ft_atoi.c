7670faac210a56ce3fcfc886ebc497bcf7d6453a
Aitor Guinea <aguinea@car6s2.42barcelona.com>
2024-10-04 12:05:43 +0200

    libft

---
+++ ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguinea <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 18:28:41 by aguinea           #+#    #+#             */
+/*   Updated: 2024/09/21 20:26:10 by aguinea          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (*nptr == 32 || (8 < *nptr && *nptr < 14))
+		nptr++;
+	while (*nptr == '-' || *nptr == '+')
+	{
+		i++;
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+		if (i >= 2)
+			return (0);
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		result = result * 10 +(*nptr - '0');
+		nptr++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+	char	*nptr = "1234";
+	int	result;
+
+	result = ft_atoi(nptr);
+	printf("%i\n", result);
+	return (0);
+}*/
